0d5f34b2ab411c9cc0e50819b38e487266b2cc61
shohag.mia <shohag.mia@bracits.com>
2024-10-19 15:47:02 +0600

    CollectDataSet

--- PRS.Service/CommonService.cs
+++ PRS.Service/CommonService.cs
@@ -20,7 +20,6 @@ namespace PRS.Service
         {
             try
             {
-                return false;
                 var trainingDataSet = await _dbContext.ModelTrainDataSets.ToListAsync();
                 if (trainingDataSet.Count > 0)
                 {
