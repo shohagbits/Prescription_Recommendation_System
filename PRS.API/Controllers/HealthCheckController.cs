1c8238540474d2b96348358bf775d4683e90f99d
shohag.mia <shohag.mia@bracits.com>
2024-11-16 11:57:31 +0600

    healthcheck

---
+++ PRS.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRS.Service;
+
+namespace PRS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("status")]
+        public async Task<IActionResult> Status()
+        {
+            return Ok($"API Health is Good at {DateTime.Now}!");
+        }
+
+    }
+}
